0ddefd2ddc3b7a6da9d5ecc3d6d4c93f4b1d3f58
George Al <gfa61.ga@gmail.com>
2019-04-30 17:38:44 +0300

    Updates

---
+++ precache-manifest.fce85b27044fb4ff7cf7647e76572055.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c350ea80cad35cfc6c21fd0e54b9cd27",
+    "url": "/react-form/index.html"
+  },
+  {
+    "revision": "6f748b5fae80f2f65c3b",
+    "url": "/react-form/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "6f748b5fae80f2f65c3b",
+    "url": "/react-form/static/js/2.41da14d5.chunk.js"
+  },
+  {
+    "revision": "3e72cc0cd8eb5296c71a",
+    "url": "/react-form/static/js/main.302807f1.chunk.js"
+  },
+  {
+    "revision": "15bae7cbdef7d06c46be",
+    "url": "/react-form/static/js/runtime~main.98e38a88.js"
+  }
+]);
